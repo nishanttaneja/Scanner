39ba9201ad150af915b749ca4eb823f4e782bedd
Nishant Taneja <nishant.taneja2701@gmail.com>
2023-08-09 01:22:56 +0530

    added What's New Screen & ISBN Barcode support

--- Scanner/ViewController.swift
+++ Scanner/ViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import AVFoundation
+import WhatsNew
 
 class ViewController: UIViewController {
     // Variables
     private let captureSession = AVCaptureSession()
-    private let captureDevice = AVCaptureDevice.default(for: .video)!
+    private let captureDevice = AVCaptureDevice.default(for: .video)
     private var video: AVCaptureVideoPreviewLayer!
+    private let metadataObjectTypes: [AVMetadataObject.ObjectType] = [.code128, .ean13, .qr]
     
     // IBAction
     @IBAction private func startScanning(_ sender: UIButton) {
@@ -22,7 +24,9 @@ class ViewController: UIViewController {
 //            imagePickerController.modalPresentationStyle = .popover
 //            present(imagePickerController, animated: true, completion: nil)
 //        }
-        captureSession.startRunning()
+        DispatchQueue.global().async {
+            self.captureSession.startRunning()
+        }
     }
 }
 
@@ -30,19 +34,27 @@ extension ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         do {
-            let captureInput = try AVCaptureDeviceInput(device: captureDevice)
-            captureSession.addInput(captureInput)
+            if let captureDevice {
+                let captureInput = try AVCaptureDeviceInput(device: captureDevice)
+                captureSession.addInput(captureInput)
+            }
         } catch {
             print(error.localizedDescription)
         }
         let captureOutput = AVCaptureMetadataOutput()
         captureSession.addOutput(captureOutput)
         captureOutput.setMetadataObjectsDelegate(self, queue: .main)
-        captureOutput.metadataObjectTypes = [.code128, .qr]
+        if captureOutput.availableMetadataObjectTypes.contains(where: { metadataObjectTypes.contains($0) }) {
+            captureOutput.metadataObjectTypes = metadataObjectTypes
+        }
         video = AVCaptureVideoPreviewLayer(session: captureSession)
         video.frame = view.frame
         view.layer.addSublayer(video)
 //        captureSession.startRunning()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        displayWhatsNew()
     }
 }
 
@@ -50,7 +62,7 @@ extension ViewController: AVCaptureMetadataOutputObjectsDelegate {
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         guard metadataObjects.count != 0,
               let object = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
-              [AVMetadataObject.ObjectType.qr, .code128].contains(object.type) else {
+              metadataObjectTypes.contains(object.type) else {
             // Error: unable to proceed for some reasons
             return
         }
@@ -61,3 +73,20 @@ extension ViewController: AVCaptureMetadataOutputObjectsDelegate {
     }
 }
 
+// MARK: - What's New
+extension ViewController: WNViewControllerDataSource {
+    private func displayWhatsNew() {
+        let controller = WNViewController()
+        controller.dataSource = self
+        present(controller, animated: true)
+    }
+    
+    // MARK: DataSource
+    func itemsForWhatsNewViewController() -> [WNItem] {
+        [
+            WNItem(image: .init(systemName: "newspaper")!, title: "What's New", description: "Discover new features. When new features are added, they will be displayed here."),
+            WNItem(image: .init(systemName: "barcode.viewfinder")!, title: "Barcode Scan", description: "Scan any Barcode. The result will be displayed in alert sheet."),
+            WNItem(image: .init(systemName: "qrcode.viewfinder")!, title: "QR Code Scan", description: "Scan any QR Code. The result will be displayed in alert sheet.")
+        ]
+    }
+}
